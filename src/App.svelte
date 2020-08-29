87e86beb760b4c20b3d7d236810687a758b86fce
Juan MG <jxithr3@hotmail.com>
2020-08-28 21:58:33 -0500

    changes

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,17 @@
 <script>
 </script>
 
+<div class="container">
+	<div class="row">
+		<div class="card col-8 mx-auto my-5">
+			<p class="h4 my-2">Add a Todo</p>
+			<div class="input-group mb-2">
+				<input class="form-control" type="text" placeholder="Enter a todo">
+				<button class="btn btn-primary" type="button">Add</button>
+			</div>
+		</div>
+	</div>
+</div>
+
 <style>
 </style>
